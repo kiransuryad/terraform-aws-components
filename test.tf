92af0bc47ad91db3e8f6f7d3e8ee038a01211bc3
kiransuryad <60929222+kiransuryad@users.noreply.github.com>
2023-04-12 12:15:24 +0100

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,13 @@
+test
+
+Dev setup & CI/CD approach agreed  
+CI/CD approach agreed
+	1. Set clear entry/exist criteria for CI/CD code, build, test, package, release, and deployment phases.
+	2. Define package management, package structure and push/pull packages
+	3. Define automation build, unit testing, code coverage and  technical debt
+	4. Define branch policies, branch merge, versioning and code reviews
+	5. Define CI/CD tools section criteria for functional/non-functional testing, customization, etc.
+	6. Define configuration management, configure stage and prevision stages 
+	
+	
+Weekly KT session with IT infra
